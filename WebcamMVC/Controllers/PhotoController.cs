7ecf4c14e41bd1de909e16e439a218c7f9daa39a
Thomas Morley <tm2412@gmail.com>
2015-10-27 19:09:54 +0000

    bug fixes

---
+++ WebcamMVC/Controllers/PhotoController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace WebcamMVC.Controllers
+{
+    public class PhotoController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Session["val"] = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string Imagename)
+        {
+            string sss = Session["val"].ToString();
+
+            ViewBag.pic = "http://localhost:55694/WebImages/" + Session["val"].ToString();
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Changephoto()
+        {
+            if (Convert.ToString(Session["val"]) != string.Empty)
+            {
+                ViewBag.pic = "http://localhost:55694/WebImages/" + Session["val"].ToString();
+            }
+            else
+            {
+                ViewBag.pic = "../../WebImages/person.jpg";
+            }
+            return View();
+        }
+
+        
+
+        public JsonResult Rebind()
+        {
+            string path = "http://localhost:55694/WebImages/" + Session["val"].ToString();
+
+            return Json(path, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult Capture()
+        {
+            var stream = Request.InputStream;
+            string dump;
+
+            using (var reader = new StreamReader(stream))
+            {
+                dump = reader.ReadToEnd();
+
+                DateTime nm = DateTime.Now;
+
+                string date = nm.ToString("yyyymmddMMss");
+
+                var path = Server.MapPath("~/WebImages/" + date + "test.jpg");
+
+                System.IO.File.WriteAllBytes(path, String_To_Bytes2(dump));
+
+                ViewData["path"] = date + "test.jpg";
+
+                Session["val"] = date + "test.jpg";
+            }
+
+            return View("Index");
+        }
+
+        private byte[] String_To_Bytes2(string strInput)
+        {
+            int numBytes = (strInput.Length) / 2;
+
+            byte[] bytes = new byte[numBytes];
+
+            for (int x = 0; x < numBytes; ++x)
+            {
+                bytes[x] = Convert.ToByte(strInput.Substring(x * 2, 2), 16);
+            }
+
+            return bytes;
+        }
+    }
+}
