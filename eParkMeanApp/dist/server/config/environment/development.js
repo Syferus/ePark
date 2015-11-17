168d0289b2434a05069b312425982d5416ca498a
Gerry Finn <s00147240@mail.itsligo.ie>
2015-11-17 00:17:51 +0000

    Mean App

---
+++ eParkMeanApp/dist/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://gerry:qwerty@ds047114.mongolab.com:47114/eparktest'
+  },
+
+  seedDB: true
+};
