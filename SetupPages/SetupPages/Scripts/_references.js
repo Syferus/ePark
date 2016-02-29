045e1ca667b660578452dd7a539c18368b6df5ca
Ciaran Durcan <Ciaran Durcan>
2016-02-29 19:21:31 +0000

    Shtuff done

--- SetupPages/SetupPages/Scripts/_references.js
+++
@@ -1,7 +0,0 @@
-﻿/// <reference path="jquery-2.2.0.js" />
-/// <autosync enabled="true" />
-/// <reference path="ai.0.15.0-build58334.min.js" />
-/// <reference path="bootstrap.min.js" />
-/// <reference path="jquery-2.2.0.min.js" />
-/// <reference path="modernizr-2.6.2.js" />
-/// <reference path="respond.js" />
